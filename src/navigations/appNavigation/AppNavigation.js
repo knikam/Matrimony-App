ed11f9cd9f5d69ba15c068b7165e6ea8f8503917
kalpesh nikam <kalpeshnikam1080@gmail.com>
2020-05-06 08:26:43 +0530

    Update

--- src/navigations/appNavigation/AppNavigation.js
+++ src/navigations/appNavigation/AppNavigation.js
@@ -20,7 +20,7 @@ function App() {
   return (
 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MatchProfile"> 
+      <Stack.Navigator initialRouteName="SplashScreen"> 
         
         <Stack.Screen name="SplashScreen" 
         component={SplashScreen}
